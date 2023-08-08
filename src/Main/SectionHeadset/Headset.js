13d29a16868f60481d39b0769579bedfd7ffc93f
Nicolas <nicolasgoulart5001@gmail.com>
2023-08-07 23:24:20 -0300

    update section headset + add products

--- src/Main/SectionHeadset/Headset.js
+++ src/Main/SectionHeadset/Headset.js
@@ -3,6 +3,10 @@ import * as S from "./Styles";
 
 //images
 import HeadsetRazer from "../../assets/headset-second-section.png"
+import Keyboard from "../../assets/teclado.png"
+import HeadsetProduct from "../../assets/headset.png"
+import Mouse from "../../assets/mouse.png"
+import Pad from "../../assets/pad.png"
 
 //component
 import ButtonComponent from "../../components/button/button";
@@ -11,16 +15,44 @@ export default function HeadsetSection(){
 
     return(
         <S.SectionHeadset>
-            <S.Content>
-                <S.HeadsetKraken src={HeadsetRazer}/>
-                <S.Texts>
-                <h1>RAZER KRAKEN PRO</h1>
-                <p>Os primeiros mouses de controle total automatizados do mundo desenvolvidos em 2016, especiais para jogos.</p>
-                <ButtonComponent
-                    textButton="VER MAIS"
-                />
-            </S.Texts>
-            </S.Content>
+            <S.SectionTwo>
+                <S.ContentHeadset>
+                    <S.HeadsetKraken src={HeadsetRazer}/>
+                    <S.Texts>
+                    <h1>RAZER KRAKEN PRO</h1>
+                    <p>Os primeiros mouses de controle total automatizados do mundo desenvolvidos em 2016, especiais para jogos.</p>
+                    <ButtonComponent
+                        textButton="VER MAIS"
+                    />
+                </S.Texts>
+                </S.ContentHeadset>
+
+                <S.ContentProducts>
+                    <S.CardProduct>
+                        <img src={Keyboard} alt="Teclado Lycosa Expert"/>
+                        <h4>Razer Lycosa Expert</h4>
+                        <p>R$975,00</p>
+                    </S.CardProduct>
+
+                    <S.CardProduct>
+                        <img src={HeadsetProduct} alt="Headset razer Kraken Pro V3"/>
+                        <h4>Razer Kraken Pro V3</h4>
+                        <p>R$436,00</p>
+                    </S.CardProduct>
+
+                    <S.CardProduct>
+                        <img src={Mouse} alt="Mouse Mamba"/>
+                        <h4>Razer Mamba</h4>
+                        <p>R$730,00</p>
+                    </S.CardProduct>
+
+                    <S.CardProduct>
+                        <img src={Pad} alt="Mousepad Razer firefly Cloth"/>
+                        <h4>Razer Firefly Cloth</h4>
+                        <p>R$142,00</p>
+                    </S.CardProduct>
+                </S.ContentProducts>
+            </S.SectionTwo>
         </S.SectionHeadset>
     )
 }
--- src/Main/SectionHeadset/Styles.js
+++ src/Main/SectionHeadset/Styles.js
@@ -1,33 +1,81 @@
 import styled from "styled-components"
 
+//Background
+import BgImageHeadset from "../../assets/bg-image-headset.png"
+import BgImageHoverProduct from "../../assets/bg-image.PNG"
+
 export const SectionHeadset = styled.section`
     background-color: #0f0f0f;
-
-`
-export const Content = styled.section`
-    max-width: 1440px;
+    background-image: url(${BgImageHeadset});
     height: 100vh;
+`
+export const SectionTwo = styled.section`
+    max-width: 1440px;
+    height: 100%;
     margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+export const ContentHeadset = styled.section`    
     display: flex;
     justify-content: space-between;
     align-items: center;
 `
 export const HeadsetKraken = styled.img`
-    width: 480px;
-    height: 520px;
+    width: 330px;
+    height: 380px;
 `
 export const Texts = styled.div`
     display: flex;
     flex-direction: column;
     gap: 50px;
     h1{
-        font-size: 3.5rem;
+        font-size: 2.8rem;
         width: 460px;
-        line-height: 60px;
+        line-height: 50px;
         letter-spacing: 5px;
     }
     p{
         color: #585858;
         width: 460px;
+    }
+`
+export const ContentProducts = styled.section`
+    height: 400px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+export const CardProduct = styled.div`
+    border: solid #2e2e2e 1px;
+    width: 300px;
+    height: 365px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    img{
+        height: 150px;
+        transition: 0.4s;
+    }
+    h4{
+        margin-top: 30px;
+        font-size: 1.3rem;
+    }
+    p{
+        color: #00ff01;
+    }
+    &:hover{
+        background-image: url(${BgImageHoverProduct});
+        cursor: pointer;
+        border: 0;
+
+        img{
+            margin-top: -15px;
+            height: 165px;
+        }
     }
 `
